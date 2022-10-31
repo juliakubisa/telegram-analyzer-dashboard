5b2aca830581e3a16c0c72ea1141e49812b1c34f
juliakubisa <julkakubisa@gmail.com>
2022-10-31 16:16:58 +0100

    interactivity

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,6 @@
+# connect to te main file
+from app import app
+
 # import libraries
 from dash import dcc, html
 import dash_bootstrap_components as dbc
@@ -19,21 +22,12 @@ import emoji
 pd.options.mode.chained_assignment = None
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-# connect to te main file
-from app import app
-
 # load data
 data = pd.read_json("/Users/julkakubisa/PycharmProjects/telegram-analysis/data/mama_result.json")
 data = pd.json_normalize(data['messages'])
 
-# drop columns
-data = data.drop(data.loc[:, ['type', 'actor', 'actor_id', 'action', 'from_id', 'photo', 'width',
-                              'height', 'reply_to_message_id', 'file', 'thumbnail', 'mime_type', 'duration_seconds',
-                              'forwarded_from', 'discard_reason',
-                              'emoticon', 'contact_vcard', 'contact_information.first_name',
-                              'contact_information.last_name', 'contact_information.phone_number',
-                              'edited', 'via_bot']], axis=1)
-data = data.drop(data.loc[:, ['id', 'sticker_emoji']], axis=1)
+# leave only important columns
+data = data.filter(['text','from', 'media_type', 'date'], axis=1)
 
 # convert date to datetime
 data["datetime"] = pd.to_datetime(data['date'])
@@ -46,10 +40,10 @@ data = data.drop(data.loc[:, ['date']], axis=1)
 data['media_type'] = data['media_type'].fillna('text')
 data = data.dropna()
 
-#extract emojis from messages
+# extract emojis from messages
 def extract_emojis(row):
     message = row.text
-    if message is None or type(message) !=str:
+    if message is None or type(message) != str:
         return None
     return ''.join(c for c in message if c in emoji.UNICODE_EMOJI['en'])
 
@@ -58,9 +52,11 @@ total_emojis = list(filter(None, data['emojis']))
 emojis_count = Counter("".join(total_emojis)).most_common(5)
 emoji_df = pd.DataFrame(emojis_count, columns=['emoji', 'count'])
 
-fig_emoji = px.pie(emoji_df, hole=.4, values='count', names='emoji', color_discrete_sequence=px.colors.sequential.Magenta)
+#figure
+fig_emoji = px.pie(emoji_df, hole=.4, values='count', names='emoji',
+                   color_discrete_sequence=px.colors.sequential.Magenta)
 fig_emoji.update_traces(textposition='inside', textinfo='percent+label')
-fig_emoji.update_layout(font = dict(size = 15))
+fig_emoji.update_layout(font=dict(size=15))
 
 # prepare text
 data["text"] = data["text"].str.lower()
@@ -72,33 +68,11 @@ data['text'] = (data['text'].astype("str")
                 .str.encode('ascii', errors='ignore')
                 .str.decode('utf-8'))
 
-# total number of messages
-sum_msg = data['text'].count()
-
-# messages distribution
-#TODO layout
-who_sent = data[['text', 'from']].groupby(['from']).count().sort_values(['text'], ascending=False)
-who_sent = who_sent.reset_index()
-
-fig1 = px.pie(who_sent, values='text', names='from',
-              color_discrete_sequence=px.colors.sequential.Magenta, labels={'text': 'no. of texts'},
-              )
-
-fig1.update_layout(
-    showlegend=False,
-    font = dict(size = 15)
-)
-
-fig1.update_traces(textposition='inside', textinfo='percent+label')
-
-
 # what type of media was sent the most
 datatype = data[['media_type', 'datetime']].groupby(['media_type']).count().sort_values(['datetime'], ascending=False)
 datatype = datatype.reset_index()
 datatype = datatype.rename(columns={'media_type': 'media type', 'datetime': 'no. messsages'})
 
-#datatype.columns('media type', 'no. messages')
-#TODO find a way to show it in the dashboard
 
 # get the number of words in messages
 def word_count(row):
@@ -118,16 +92,12 @@ people = data_wc['from'].unique()
 for name in people:
     user_data = data_wc[data_wc["from"] == name]
     words_per_message = np.sum(user_data['word_count'])
-    #print(name, 'sent ', int(words_per_message), ' words, average ', round(words_per_message / user_data.shape[0], 2),
+    # print(name, 'sent ', int(words_per_message), ' words, average ', round(words_per_message / user_data.shape[0], 2),
     #    ' words per message')
 
 # number of messages per day
 date_df = data.resample("D").apply({'text': 'count'})
 date_df.reset_index(inplace=True)
-
-# average messages per day
-sum_days = len(date_df.index)
-avg_mess = round(sum_msg / sum_days, 2)
 
 # most active day
 day = date_df.max()
@@ -160,9 +130,10 @@ def week_name(i):
     l = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     return l[i]
 
+
 week['day_week_num'] = week['day_week_num'].apply(week_name)
 
-#TODO layout
+# TODO layout
 fig5 = px.bar_polar(week, r="text", theta="day_week_num",
                     color="text", template="plotly_white", labels={'text': 'messages'},
                     color_continuous_scale=px.colors.sequential.Magenta)
@@ -188,15 +159,15 @@ def tokenize(text):
 
 df2['tokenized'] = df2['text'].apply(tokenize)
 
-
 # split words
 splitted = " ".join(df2['tokenized']).split()
+
 
 # wordcloud
 # cloud = WordCloud(background_color='white', max_font_size=150,
 #                   ).generate(df2['tokenized'].to_string())
 
-#TODO: show wordcloud
+# TODO: show wordcloud
 
 # ngrams
 def ngrams_df(num_grams, splitted):
@@ -209,12 +180,13 @@ def ngrams_df(num_grams, splitted):
 
     return all_grams
 
+
 all_grams_df = ngrams_df(2, splitted)
 
 fig_ngrams = make_subplots(rows=2, cols=1)
 
 fig_ngrams.add_trace(go.Bar(
-    name = "Unigrams",
+    name="Unigrams",
     marker={'color': '#dd5182'},
     y=all_grams_df[1].str[0].astype('str'),
     x=all_grams_df[1].str[1],
@@ -222,7 +194,7 @@ fig_ngrams.add_trace(go.Bar(
     row=1, col=1)
 
 fig_ngrams.add_trace(go.Bar(
-    name = "Bigrams",
+    name="Bigrams",
     marker={'color': '#e9a9bd'},
     y=all_grams_df[2].str[0].astype('str'),
     x=all_grams_df[2].str[1],
@@ -235,21 +207,18 @@ fig_ngrams.update_layout(
     font_size=10,
     template="plotly_white",
     legend=dict(
-    orientation="h",
-    yanchor="bottom",
-    y=1.02,
-    xanchor="right",
-    x=1
-)
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+    )
 )
 
 fig_ngrams.for_each_xaxis(lambda x: x.update(showgrid=False))
 fig_ngrams.for_each_yaxis(lambda x: x.update(showgrid=False))
-
-
 fig_ngrams.update_traces(textfont_size=8)
-
-#TODO
+# TODO
 
 # LAYOUT
 
@@ -257,17 +226,17 @@ fig_ngrams.update_traces(textfont_size=8)
 header = dbc.Row([
     html.H1(children="Telegram Analyzer", className="header-title"),
     html.Div([
-    dcc.DatePickerRange(
-        id="date-range",
-        min_date_allowed=data.datetime.min().date(),
-        max_date_allowed=data.datetime.max().date(),
-        start_date=data.datetime.min().date(),
-        end_date=data.datetime.max().date(),
-        minimum_nights = 30, #TODO monthly distribution error
-        display_format='DD/MM/YYYY',
-    ),
-        ],
-        className= "date-picker"
+        dcc.DatePickerRange(
+            id="date-range",
+            min_date_allowed=data.datetime.min().date(),
+            max_date_allowed=data.datetime.max().date(),
+            start_date=data.datetime.min().date(),
+            end_date=data.datetime.max().date(),
+            minimum_nights=30,  # TODO monthly distribution error
+            display_format='DD/MM/YYYY',
+        ),
+    ],
+        className="date-picker"
     ),
 ],
     className="header"
@@ -278,25 +247,25 @@ statistics = dbc.Row(
     [
         dbc.Col([
             html.Div(children="Tot. messages", className="stats-title"),
-            html.P(children=sum_msg, className="stats-description"),
+            html.P(className="stats-description", id="tot_mess"),
         ]),
         dbc.Col([
             html.Div(children="Avg. messages/day", className="stats-title"),
-            html.P(children=avg_mess, className="stats-description"),
+            html.P(className="stats-description", id="avg_mess"),
         ]),
         dbc.Col([
             html.Div(children="Avg. words", className="stats-title"),
-            html.P(children="3.52", className="stats-description"),
+            html.P(className="stats-description", id="avg_words"),
         ]),
         dbc.Col([
             html.Div(children="Most active day", className="stats-title"),
-            html.P(children=exact_day, className="stats-description"),
+            html.P(className="stats-description", id="exact_day"),
         ]),
     ],
     className="stats"
 )
 
-#timeline
+# timeline
 timeline = dbc.Row([
     html.Div([
         html.I(className="bi bi-star-fill"), " Chat timeline",
@@ -306,7 +275,6 @@ timeline = dbc.Row([
     dcc.Graph(id="timeline_graph")
 ],
 )
-
 
 # body
 body = dbc.Row(
@@ -318,7 +286,7 @@ body = dbc.Row(
                 ],
                     className="graph-title"
                 ),
-                dcc.Graph(figure=fig1)
+                dcc.Graph(id="mess_distr")
             ],
             ),
             dbc.Row([
@@ -337,7 +305,7 @@ body = dbc.Row(
                     className="graph-title"
                 ),
                 dcc.Graph(figure=fig_emoji)
-                #TODO pick the number of emojis shown
+                # TODO pick the number of emojis shown
             ],
             ),
         ],
@@ -371,36 +339,73 @@ body = dbc.Row(
         ),
         dbc.Col([
             html.Div([
-            dbc.Table.from_dataframe(datatype, hover=True, className="table-style", index=False)
-                ],
+                dbc.Table.from_dataframe(datatype, hover=True, className="table-style", index=False)
+            ],
                 className='tables-container'
             ),
             html.Div([
                 html.Div([
-                html.I(className="bi bi-star-fill"), " Messages per month",
-                    ],
+                    html.I(className="bi bi-star-fill"), " Messages per month",
+                ],
                     className="graph-title"
                 ),
                 dcc.Graph(figure=fig_ngrams)
-                ],
+            ],
                 className='ngrams-container'
             ),
         ],
             md=2
-       ),
+        ),
     ],
     justify="center",
 )
 
 
-# layout
-app.layout = dbc.Container(fluid=True, children=[header, statistics, timeline, body],
-                           style={'background-color': '#FFFFFF'})
+# stats
+@app.callback(
+    [Output("tot_mess", "children"), Output("avg_mess", "children"), Output("exact_day", "children"),
+     Output("mess_distr", "figure")],
+    [
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    ],
+)
+
+def update_stats(start_date, end_date):
+    mask = (
+            (data.datetime >= start_date)
+            & (data.datetime <= end_date)
+    )
+    filtered_data = data.loc[mask, :]
+    date_df = filtered_data.resample("D").apply({'text': 'count'}) #messages counted by days
+    date_df.reset_index(inplace=True)
+
+    who_sent = filtered_data[['text', 'from']].groupby(['from']).count().sort_values(['text'], ascending=False) #who sent how many
+    who_sent = who_sent.reset_index()
+
+    #statistics
+    tot_mess = filtered_data['text'].count() #total messages
+    sum_days = len(date_df.index) #total number of days
+    avg_mess = round(tot_mess / sum_days, 2) #average messages per day
+    day = date_df.max()
+    exact_day = day[0].strftime("%d/%m/%Y")
+
+    #who sent chart
+    mess_distr = px.pie(who_sent, values='text', names='from',
+                  color_discrete_sequence=px.colors.sequential.Magenta, labels={'text': 'no. of texts'},
+                  )
+
+    mess_distr.update_layout(
+        showlegend=False,
+        font=dict(size=15)
+    )
+
+    mess_distr.update_traces(textposition='inside', textinfo='percent+label')
+
+    return tot_mess, avg_mess, exact_day, mess_distr
 
 
-#callbacks
-
-#timeline
+# timeline
 @app.callback(
     Output("timeline_graph", "figure"),
     [
@@ -408,25 +413,25 @@ app.layout = dbc.Container(fluid=True, children=[header, statistics, timeline, b
         Input("date-range", "end_date"),
     ],
 )
-
 def update_timeline(start_date, end_date):
     mask = (
-        (date_df.datetime >= start_date)
-        & (date_df.datetime<= end_date)
+            (date_df.datetime >= start_date)
+            & (date_df.datetime <= end_date)
     )
     filtered_data = date_df.loc[mask, :]
     timeline_graph = px.line(filtered_data, x="datetime", y="text",
-                   color_discrete_sequence=px.colors.sequential.Magenta,
-                   labels={"datetime": "date", "text": "messages"},
-                   template="plotly_white")
+                             color_discrete_sequence=px.colors.sequential.Magenta,
+                             labels={"datetime": "date", "text": "messages"},
+                             template="plotly_white")
 
     timeline_graph.update_layout(title={'x': 0.5},
-                       margin_b=60,
-                       margin_r=30, )
+                                 margin_b=60,
+                                 margin_r=30, )
 
     return timeline_graph
 
-#monthly distribution
+
+# monthly distribution
 @app.callback(
     Output("monthly_distr", "figure"),
     [
@@ -434,28 +439,30 @@ def update_timeline(start_date, end_date):
         Input("date-range", "end_date"),
     ],
 )
-
-
 def update_monthly(start_date, end_date):
     mask = (
-        (date_df_m.datetime >= start_date)
-        & (date_df_m.datetime<= end_date)
+            (date_df_m.datetime >= start_date)
+            & (date_df_m.datetime <= end_date)
     )
 
     filtered_data_m = date_df_m.loc[mask, :]
     monthly_distr = px.area(filtered_data_m, x="datetime", y="text",
-                   color_discrete_sequence=px.colors.sequential.Magenta,
-                   labels={"datetime": "date", "text": "messages"},
-                   template="plotly_white")
+                            color_discrete_sequence=px.colors.sequential.Magenta,
+                            labels={"datetime": "date", "text": "messages"},
+                            template="plotly_white")
 
     monthly_distr.update_layout(title={'x': 0.5},
-                       margin_b=60,
-                       margin_r=30, )
+                                margin_b=60,
+                                margin_r=30, )
     return monthly_distr
 
-#TODO upload files
+
+# TODO upload files
+
+# layout
+app.layout = dbc.Container(fluid=True, children=[header, statistics, timeline, body],
+                           style={'background-color': '#FFFFFF'})
 
 # run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
-
